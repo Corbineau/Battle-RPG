99cc95deb83cdf2bfba5a8c33d6ae819c3dcab7c
Rabbit <Corbineau@users.noreply.github.com>
2019-03-21 16:59:36 -0400

    All functionality complete! Well, except for resetting the dom. Maybe I should just make it refresh the page. ><

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,136 +22,151 @@ var game = {
     gameOn: false,
     player: "",
     activeOpponent: "",
-    availableOpponents: 4,
+    availableOpponents: 3,
+    oppHandle: "",
 
-    
 
-    gameStart: function(charId, healthDiv) {
+
+    gameStart: function (charId, healthDiv) {
         //start the game
         this.gameOn = true;
         console.log(this.gameOn);
         //set the character; add constructor logic; move the image to the battlefield
-        if(charId === "dw") {
-            this.player = new Character("Dungeon World", 245, 17, charId, healthDiv );
+        if (charId === "dw") {
+            this.player = new Character("Dungeon World", 245, 17, charId, healthDiv);
             var charImg = $("#1w").detach();
             charImg.appendTo("#battlefield");
-        } else if (charId === "paranoia" ) {
-            this.player = new Character("Fate", 520, 9, charId, healthDiv );
+        } else if (charId === "paranoia") {
+            this.player = new Character("Fate", 520, 9, charId, healthDiv);
             var charImg = $("#2w").detach();
             charImg.appendTo("#battlefield");
-        } else if (charId === "fate" ) {
-            this.player = new Character("Paranoia", 333, 2, charId, healthDiv );
+        } else if (charId === "fate") {
+            this.player = new Character("Paranoia", 333, 2, charId, healthDiv);
             var charImg = $("#3w").detach();
             charImg.appendTo("#battlefield");
         } else if (charId === "dnd") {
-                this.player = new Character("Dungeons & Dragons", 750, 10, charId, healthDiv );
-                var charImg = $("#4w").detach();
+            this.player = new Character("Dungeons & Dragons", 750, 10, charId, healthDiv);
+            var charImg = $("#4w").detach();
             charImg.appendTo("#battlefield");
         }
         console.log(this.player);
         //change the game message to ask player to pick an opponent
         $("#charMessage").text("Choose your opponent.");
-        
+
     },
 
-    setOppponent: function(oppId, healthDiv) {
+    setOppponent: function (oppId, healthDiv) {
         //check to make sure the player hasn't clicked themselves.
-        if(oppId != this.player.id) {
-            //set the active opponent; add constructor logic; move the image to the battlefield
-            if(oppId === "dw") {
+        if (oppId != this.player.id) {
+            //set the active opponent; add constructor logic
+            if (oppId === "dw") {
                 this.activeOpponent = new Opponent("Dungeon World", 245, 25, healthDiv);
-                var charImg = $("#1w").detach();
-               // charImg.appendTo("#battlefield");
-            } else if (oppId === "paranoia" ) {
+                this.oppHandle = $("#1w").detach();
+            } else if (oppId === "paranoia") {
                 this.activeOpponent = new Opponent("Paranoia", 520, 35, healthDiv);
-                var charImg = $("#2w").detach();
-                //charImg.appendTo("#battlefield");
-            } else if (oppId === "fate" ) {
+                this.oppHandle = $("#2w").detach();
+            } else if (oppId === "fate") {
                 this.activeOpponent = new Opponent("Fate", 333, 50, healthDiv);
-                var charImg = $("#3w").detach();
-                //charImg.appendTo("#battlefield");
+                this.oppHandle = $("#3w").detach();
             } else if (oppId === "dnd") {
-                    this.activeOpponent = new Opponent("Dungeons & Dragons", 750, 27, healthDiv);
-                    var charImg = $("#4w").detach();
-                //charImg.appendTo("#battlefield");
+                this.activeOpponent = new Opponent("Dungeons & Dragons", 750, 27, healthDiv);
+                this.oppHandle = $("#4w").detach();
+            }
+
         }
-        
-        }
-        charImg.appendTo("#battlefield");
-        console.log(this.activeOpponent);
+        //move the opponent to the battlefield
+        this.oppHandle.appendTo("#battlefield");
+        //reduce the number of available opponents
+        this.availableOpponents--;
+        console.log(this.activeOpponent, `you have ${this.availableOpponents} left to fight.`);
         //change the game message to ready for battle
         $("#charMessage").text("Get ready for battle!");
         //add the attack button
-        if(oppId != this.player.id) {
+        if ((oppId != this.player.id) && (document.querySelectorAll("#attack").length < 1)) {
             $("#gameMessage").append("<button id='attack'>attack!</button>");
-
         }
-        
-    },  
+    },
+
     //fight!
-    combat: function() {
+    combat: function () {
+        //enemy takes damage
         this.activeOpponent.healthPoints = (this.activeOpponent.healthPoints - this.player.attackPower);
-        if(this.activeOpponent.healthPoints >= 0) {
+        //If the opponent isn't dead, they can counterattack; update everyone's health
+        if (this.activeOpponent.healthPoints >= 0) {
             this.activeOpponent.healthDiv.text(this.activeOpponent.healthPoints);
             this.player.healthPoints = (this.player.healthPoints - this.activeOpponent.counterAttackPower);
             this.player.healthDiv.text(this.player.healthPoints);
+            //update attack power only if the opponent isn't dead
             this.player.attackPower = (this.player.attackPower + this.player.attackPower);
-        } else {
+        } else if (this.activeOpponent.healthPoints <= 0) {
+            //avoid showing overkill
             this.activeOpponent.healthDiv.text("0");
         }
-        
+
         console.log(this.player.attackPower);
     },
-
-    combatCheck: function() {
-        if(this.player.healthPoints <= 0){
+    //checks for the end of combat, and whether the game is over
+    combatCheck: function () {
+        if (this.player.healthPoints <= 0) {
             this.activeOpponent = "";
+            this.player.healthDiv.text("0");
             $("#attack").attr("id", "restart").text("restart");
-            $("#charMessage").text("Game Over! Click restart to try again."); 
-        } else if((this.activeOpponent <=0) ){
-
+            $("#charMessage").text("Game Over! Click restart to try again.");
+        } else if ((this.activeOpponent.healthPoints <= 0) && (this.availableOpponents > 0)) {
+            $(this.oppHandle).detach();
+            this.activeOpponent = "";
+            this.oppHandle = "";
+            $("#charMessage").text("Well done! Choose your next opponent.");
+        } else if ((this.activeOpponent.healthPoints <= 0)) {
+            $(this.oppHandle).detach();
+            this.activeOpponent = "";
+            this.oppHandle = "";
+            $("#attack").attr("id", "restart").text("restart");
+            $("#charMessage").text("You win! Click restart to play again!");
         }
     },
 
-    gameInit: function() {
+    gameInit: function (startState) {
         this.gameOn = false;
         this.player = "";
-        
+        this.availableOpponents = 3;
+        $(document).replaceWith(startState.clone(true));
+
     },
 }
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
+
     //choose the player character
-    $(".character").on("click", function(){
-        if(game.gameOn === false) {
+    $(".character").on("click", function () {
+        if (game.gameOn === false) {
             var charId = $(this).attr("id");
             var healthDiv = $(`#${$(this).siblings('.hp').attr('id')}`);
             console.log(charId);
             console.log(healthDiv);
             game.gameStart(charId, healthDiv);
             //choose the active opponent
-        } else if(game.activeOpponent === "") {
+        } else if (game.activeOpponent === "") {
             var oppId = $(this).attr("id");
             var healthDiv = $(`#${$(this).siblings('.hp').attr('id')}`);
             game.setOppponent(oppId, healthDiv);
-            
-        } 
-    })
+
+        }
+    });
     //combat clicker
-    $(document).on("click", "#attack", function(){
-        console.log("attack!")
+    $(document).on("click", "#attack", function () {
         game.combat();
         game.combatCheck();
-    })
+    });
 
     //restart game
-    $(document).on("click", "#restart", function(){
-        game.gameInit();
+    $(document).on("click", "#restart", function () {
+        game.gameInit(startState);
         console.log("resetting", game);
-        $("#document").replaceWith(divClone.clone(true));
-    })
+    });
 
-    var divClone = $("#document").clone(true);
+    var startState = $(document).clone(true);
+
 })
 
